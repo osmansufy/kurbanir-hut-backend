961b5c213ea253c832082e6417af02e5f4f7de6f
osmansufy <osmansufy20@gmail.com>
2023-06-18 21:53:19 +0600

    deploy on vercel

--- dist/src/main/modules/cow/cow.constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.priceFilter = exports.cowFilterableFields = exports.cowSearchableFields = void 0;
-exports.cowSearchableFields = ["location", "breed", "category"];
-exports.cowFilterableFields = ["searchTerm", "location"];
-exports.priceFilter = ["minPrice", "maxPrice"];
--- dist/src/main/modules/order/order.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrderStatus = void 0;
-var OrderStatus;
-(function (OrderStatus) {
-    OrderStatus["PENDING"] = "pending";
-    OrderStatus["COMPLETED"] = "completed";
-    OrderStatus["CANCELED"] = "canceled";
-})(OrderStatus || (exports.OrderStatus = OrderStatus = {}));
